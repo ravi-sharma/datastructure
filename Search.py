5063ca72452cda80f40e81fc37bd78a6fb3ed882
Ravi Sharma <ravi@driveyello.com>
2019-09-01 14:01:16 +1000

    - Adding binary search

---
+++ Search.py
@@ -0,0 +1,47 @@
+class Search:
+    POSITION = 0
+
+    # linear search with algorithm O(n)
+    def linear(self, list, number):
+        for i in range(len(list)):
+            if list[i] == number:
+                self.POSITION = i
+                return True
+
+        return False
+
+    # linear search with algorithm O(n)
+    def binary(self, list, number):
+        lowest = 0
+        upper = len(list) - 1
+
+        while lowest <= upper:
+            mid = (lowest + upper) // 2  # find mid
+
+            if list[mid] == number:
+                self.POSITION = mid
+                return True
+            else:
+                if list[mid] < number: 
+                    lowest = mid + 1
+                else:
+                    upper = mid - 1
+
+        return False
+
+
+list = [1, 2, 3, 4, 5, 8, 10]
+
+number = 18
+
+object = Search()
+
+if object.linear(list, number):
+    print('Found on', object.POSITION)
+else:
+    print('Not found!')
+
+if object.binary(list, number):
+    print('Found on', object.POSITION)
+else:
+    print('Not found!')
